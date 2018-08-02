5fb1eff7cf9d5da39c8d6258618abfdfbede6888
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-02 14:26:53 +0200

    HelloOthers added

---
+++ week-01/day-04/src/HelloOthers.java
@@ -0,0 +1,8 @@
+public class HelloOthers {
+  public static void main(String[] args) {
+
+    System.out.println("Hello, Archie!");
+    System.out.println("Hello, Osama!");
+    System.out.println("Hello, Ben!");
+  }
+}
