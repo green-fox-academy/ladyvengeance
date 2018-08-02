9e16a0d128b29caa55feed74b98ae5c588c144a6
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-02 17:13:45 +0200

    SecondsInADay added

---
+++ week-01/day-04/src/SecondsInADay.java
@@ -0,0 +1,11 @@
+public class SecondsInADay {
+  public static void main(String[] args) {
+    int currentHours = 14;
+    int currentMinutes = 34;
+    int currentSeconds = 42;
+
+    int remainingSeconds = (24 - currentHours - 1) * 360 + (60 - currentMinutes - 1) * 60 + (60 - currentSeconds);
+
+    System.out.println(remainingSeconds);
+  }
+}
