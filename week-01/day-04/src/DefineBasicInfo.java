75d765d8055aff6ed6dc04e8ad60928bbc9bc32a
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-02 16:29:11 +0200

    DefineBasicInfo added

---
+++ week-01/day-04/src/DefineBasicInfo.java
@@ -0,0 +1,14 @@
+public class DefineBasicInfo {
+  public static void main(String[] args) {
+    String name = "Deszpoth Csilla";
+    int age = 30;
+    double height = 1.64;
+    boolean isMarried = false;
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(height);
+    System.out.println(isMarried);
+
+  }
+}
