c3f4e27258e12acc9d3140653c9060b6b36c7aae
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-02 14:33:21 +0200

    IntroduceYourself added

---
+++ week-01/day-04/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+  public static void main(String[] args) {
+    System.out.println("I'm C");
+    System.out.println("30");
+    System.out.println("1.64");
+  }
+}
