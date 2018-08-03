5277d50a2e74a7d65659bdf7a9e4657629041332
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-03 13:17:52 +0200

    finally got MileToKMConverter a.k.a in yo face!

--- week-01/day-04/src/MileToKmConverter.java
+++ week-01/day-04/src/MileToKmConverter.java
@@ -1,13 +1,21 @@
-import java.util.Scanner
+import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class MileToKmConverter {
+  private static DecimalFormat df2 = new DecimalFormat(".##");
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
     System.out.println("Please type a whole number as a distance in km");
     int userInput1 = scanner.nextInt();
-    float kmToMile = userInput1 * 0.62137;
-    System.out.println(userInput1 + " is " + kmToMile);
+    double kmToMile = userInput1 * 0.621371;
+    if (userInput1 == 1) {
+      System.out.println(userInput1 + " km is " + String.format(java.util.Locale.US,"%.2f", kmToMile) + " miles.");
+    }
+    else {
+      System.out.println(userInput1 + " km are " + String.format(java.util.Locale.US,"%.2f",kmToMile) + " miles.");
+    }
+
 
   }
 }
