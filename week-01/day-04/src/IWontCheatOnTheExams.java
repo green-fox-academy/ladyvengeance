b7d7158eb51874f0675cffad289b9bf31c934cbe
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-03 14:08:35 +0200

    IWontCheatOnTheExams added

---
+++ week-01/day-04/src/IWontCheatOnTheExams.java
@@ -0,0 +1,8 @@
+public class IWontCheatOnTheExams {
+  public static void main(String[] args) {
+    String sentence = "I won't cheat on the exam!";
+    for (int i = 0; i < 100; i++) {
+      System.out.println(sentence);
+    }
+  }
+}
