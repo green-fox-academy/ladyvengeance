c8bbede9c6e8d0be44b4190ee0229e7b70744b5e
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-06 15:38:04 +0200

    CompareLength added

---
+++ week-02/day-01/src/CompareLength.java
@@ -0,0 +1,12 @@
+public class CompareLength {
+  public static void main(String[] args) {
+    int[] p1 = {1, 2, 3};
+    int[] p2 = {4, 5};
+
+    if (p2.length > p1.length) {
+      System.out.println("True");
+    } else {
+      System.out.println("False");
+    }
+  }
+}
