bbf25832e842ee0e06c4b8166add05f95bdeb30c
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-06 14:37:39 +0200

    Factorio added

--- week-02/day-01/src/Factorio.java
+++ week-02/day-01/src/Factorio.java
@@ -4,8 +4,15 @@ public class Factorio {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Please type in a number and I will ");
+    System.out.println("Please type in a number and I will let you know its factorial");
     int userinput = scanner.nextInt();
+
+    System.out.println(factorio(userinput));
   }
-  public static int factorio(int x)
+  public static int factorio(int x) {
+    int temp = 1;
+    for (int i = 1; i <= x ; i++) {
+      temp = temp * i;
+    } return temp;
+  }
 }
