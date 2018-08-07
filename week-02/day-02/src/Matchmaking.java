428f576c053e2d0569459655cd5e0f3018ebf534
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-07 14:43:13 +0200

    TakesLonger modified

---
+++ week-02/day-02/src/Matchmaking.java
@@ -0,0 +1,15 @@
+//import java.util.*;
+//
+//public class Matchmaking{
+//  public static void main(String... args){
+//    ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve","Ashley","Claire","Kat","Jane"));
+//    ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe","Fred","Tom","Todd","Neef","Jeff"));
+//
+//    System.out.println(makingMatches((girls, boys));
+//  }
+//  public static ArrayList<String> makingMatches(ArrayList<String> input) {
+//    for (int i = 0; i < 5; i++) {
+//      return input;
+//    }
+//  }
+//}
--- week-02/day-02/src/TakesLonger.java
+++ week-02/day-02/src/TakesLonger.java
@@ -3,7 +3,7 @@ public class TakesLonger {
     String quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
 
     StringBuilder quoteFixed = new StringBuilder(quote);
-    quoteFixed.insert(21,"always takes longer than ");
+    quoteFixed.insert(quote.indexOf("you"),"always takes longer than ");
 
     System.out.println(quoteFixed);
   }
