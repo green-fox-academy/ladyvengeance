972929383bc70e4a2da26204f28ec590a651965d
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-07 14:57:33 +0200

    Matchmaking added

--- week-02/day-02/src/Matchmaking.java
+++ week-02/day-02/src/Matchmaking.java
@@ -1,15 +1,19 @@
-//import java.util.*;
-//
-//public class Matchmaking{
-//  public static void main(String... args){
-//    ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve","Ashley","Claire","Kat","Jane"));
-//    ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe","Fred","Tom","Todd","Neef","Jeff"));
-//
-//    System.out.println(makingMatches((girls, boys));
-//  }
-//  public static ArrayList<String> makingMatches(ArrayList<String> input) {
-//    for (int i = 0; i < 5; i++) {
-//      return input;
-//    }
-//  }
-//}
+import java.util.*;
+
+public class Matchmaking{
+  public static void main(String... args){
+    ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve","Ashley","Claire","Kat","Jane"));
+    ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe","Fred","Tom","Todd","Neef","Jeff"));
+
+    System.out.println(makingMatches(girls, boys));
+  }
+  public static ArrayList<String> makingMatches(ArrayList<String> input1, ArrayList<String> input2) {
+    ArrayList<String> output = new ArrayList<>();
+
+    for (int i = 0; i < input1.size(); i++) {
+      output.add(input1.get(i));
+      output.add(input2.get(i));
+    }
+    return output;
+  }
+}
