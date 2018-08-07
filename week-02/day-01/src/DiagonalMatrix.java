e8c610d8527cc75a66c807dca79d2f8be1589380
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-07 11:58:49 +0200

    DiagonalMatrix not finished, SimpleReplace added

--- week-02/day-01/src/DiagonalMatrix.java
+++ week-02/day-01/src/DiagonalMatrix.java
@@ -4,5 +4,19 @@ public class DiagonalMatrix {
         {0},
         {1}
     };
+    for (int i = 1; i <= num; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (i > 1 && i < n && j > 1 && j < n) {
+          if (i == j) {
+            System.out.print("%");
+          } else {
+            System.out.print(" ");
+          }
+        } else {
+          System.out.print("%");
+        }
+      }
+      System.out.println();
+    }
   }
 }
