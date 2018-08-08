b1e9bd5a464946deb8572d7a2da4220c4fa66ded
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-08 17:20:12 +0200

    PositionSquare added

--- week-02/day-03/src/PositionSquare.java
+++ week-02/day-03/src/PositionSquare.java
@@ -2,28 +2,29 @@ import javax.swing.*;
 import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class FourRectangles {
+public class PositionSquare {
+
   public static void mainDraw(Graphics graphics){
 
     int max = 220;
     int min = 20;
 
-    int width = 20;
-    int height = 30;
+    int x = (int)(Math.random() * ((max-min) + 1)) + min;
+    int y = (int)(Math.random() * ((max-min) + 1)) + min;
 
-    graphics.setColor(Color.RED);
-    graphics.drawRect((int)(Math.random() * ((max-min) + 1)) + min, (int)(Math.random() * ((max-min) + 1)) + min, width, height);
-    graphics.setColor(Color.GREEN);
-    graphics.drawRect((int)(Math.random() * ((max-min) + 1)) + min, (int)(Math.random() * ((max-min) + 1)) + min, width, height);
-    graphics.setColor(Color.BLUE);
-    graphics.drawRect((int)(Math.random() * ((max-min) + 1)) + min, (int)(Math.random() * ((max-min) + 1)) + min, width, height);
-    graphics.setColor(Color.MAGENTA);
-    graphics.drawRect((int)(Math.random() * ((max-min) + 1)) + min, (int)(Math.random() * ((max-min) + 1)) + min, width, height);
+    for (int i = 1; i < 4; i++) {
+      drawSquare(x * i, y * i, graphics);
+    }
+  }
+
+  public static void drawSquare (int x, int y, Graphics graphics) {
+    graphics.drawRect(x, y, 50, 50);
   }
 
   //    Don't touch the code below
   static int WIDTH = 320;
   static int HEIGHT = 343;
+
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
     jFrame.setSize(new Dimension(WIDTH, HEIGHT));
