0c55f25997e609967fc3888cd3904f95b3beab7a
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-08 15:02:29 +0200

    HorizontalLines added

---
+++ week-02/day-03/src/HorizontalLines.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class HorizontalLines {
+  public static void mainDraw(Graphics graphics) {
+    // Create a line drawing function that takes 2 parameters:
+    // The x and y coordinates of the line's starting point
+    // and draws a 50 long horizontal line from that point.
+    // Draw 3 lines with that function. Use loop for that.
+    for (int i = 1; i < 4; i++) {
+      drawHorizontal(30, i * 20, graphics);
+    }
+
+
+  }
+  public static void drawHorizontal (int x, int y, Graphics graphics) {
+    graphics.drawLine(x, y, x + 50, y);
+  }
+
+  // Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 343;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
