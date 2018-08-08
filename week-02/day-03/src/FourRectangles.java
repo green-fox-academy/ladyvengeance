f15af08486f0279a3cb24df65cb47e6f8e823651
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-08 16:19:58 +0200

    FourRectangles added

---
+++ week-02/day-03/src/FourRectangles.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class FourRectangles {
+  public static void mainDraw(Graphics graphics){
+
+    int width = 20;
+    int height = 30;
+
+    int max = 220;
+    int min = 20;
+
+    graphics.setColor(Color.RED);
+    graphics.drawRect((int)(Math.random()*((max-min)+1))+min, (int)(Math.random()*((max-min)+1))+min, width, height);
+    graphics.setColor(Color.GREEN);
+    graphics.drawRect((int)(Math.random()*((max-min)+1))+min, (int)(Math.random()*((max-min)+1))+min, width, height);
+    graphics.setColor(Color.BLUE);
+    graphics.drawRect((int)(Math.random()*((max-min)+1))+min, (int)(Math.random()*((max-min)+1))+min, width, height);
+    graphics.setColor(Color.MAGENTA);
+    graphics.drawRect((int)(Math.random()*((max-min)+1))+min, (int)(Math.random()*((max-min)+1))+min, width, height);
+  }
+
+  //    Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 343;
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
