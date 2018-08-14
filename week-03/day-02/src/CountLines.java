3629f50218f632e4f13d4ac374824203450548a0
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-14 12:00:02 +0200

    CountLines started

---
+++ week-03/day-02/src/CountLines.java
@@ -0,0 +1,12 @@
+public class CountLines {
+  public static void main(String[] args) {
+    // Write a function that takes a filename as string,
+    // then returns the number of lines the file contains.
+    // It should return zero if it can't open the file, and
+    // should not raise any error.
+
+  }
+  public static int numOfLines (String x) {
+    
+  }
+}
