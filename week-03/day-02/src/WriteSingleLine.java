1047a1a2cf47df2811bb7a48e6da79e9de4bd08c
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-14 14:29:28 +0200

    WriteSingleLine finished

--- week-03/day-02/src/WriteSingleLine.java
+++ week-03/day-02/src/WriteSingleLine.java
@@ -1,6 +1,7 @@
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.Files;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class WriteSingleLine {
   public static void main(String[] args) {
@@ -9,6 +10,17 @@ public class WriteSingleLine {
     // If the program is unable to write the file,
     // then it should print an error message like: "Unable to write file: my-file.txt"
 
+    addNewLine("my-file.txt");
+  }
+  public static void addNewLine (String x) {
 
+    try {
+      Writer writer = new BufferedWriter(new OutputStreamWriter(
+          new FileOutputStream(x, true), "UTF-8"));
+      writer.append("\nDeszpoth Csilla");
+      writer.close();
+    } catch (Exception e) {
+      System.out.println("Unable to read file: my-file.txt");
+    }
   }
 }
