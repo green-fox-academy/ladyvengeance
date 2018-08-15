b1e4609203b9a4e447b6b648c2d2d30ab9eda3ae
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-15 17:27:00 +0200

    Dominoes finally finished, thanks a mil Aze T_T

---
+++ week-03/day-03/DiceSet/src/DiceSet.java
@@ -0,0 +1,45 @@
+public class DiceSet {
+  // You have a `DiceSet` class which has a list for 6 dice
+  // You can roll all of them with roll()
+  // Check the current rolled numbers with getCurrent()
+  // You can reroll with reroll()
+  // Your task is to roll the dice until all of the dice are 6
+  int[] die = new int[6];
+
+  public int[] roll() {
+    for (int i = 0; i < die.length; i++) {
+      die[i] = (int) (Math.random() * 6) + 1;
+    }
+    return die;
+  }
+
+  public int[] getCurrent() {
+    return die;
+  }
+
+  public int getCurrent(int i) {
+    return die[i];
+  }
+
+  public void reroll() {
+    for (int i = 0; i < die.length; i++) {
+      die[i] = (int) (Math.random() * 6) + 1;
+    }
+  }
+
+  public void reroll(int k) {
+    die[k] = (int) (Math.random() * 6) + 1;
+  }
+
+  public static void main(String[] args) {
+    DiceSet diceSet = new Dice();
+    diceSet.getCurrent();
+    diceSet.roll();
+    diceSet.getCurrent();
+    diceSet.getCurrent(5);
+    diceSet.reroll();
+    diceSet.getCurrent();
+    diceSet.reroll(4);
+    diceSet.getCurrent();
+  }
+}
--- week-03/day-03/Dominoes/src/Dominoes.java
+++ week-03/day-03/Dominoes/src/Dominoes.java
@@ -9,11 +9,26 @@ public class Dominoes {
     // eg: [2, 4], [4, 3], [3, 5] ...
 
     System.out.println(dominoes);
+    System.out.println(sortDominoes(dominoes));
   }
 
+  static List<Domino> sortDominoes(List<Domino> unsorted) {
+    List<Domino> sortDominoes = new ArrayList<>();
+    sortDominoes.add(unsorted.get(0));
+
+    for (int i = 0; sortDominoes.size() != unsorted.size(); i++) {
+      for (int j = 0; j < unsorted.size(); j++) {
+        if (sortDominoes.get(i).getValues()[1] == unsorted.get(j).getValues()[0]) {
+          sortDominoes.add(unsorted.get(j));
+        }
+      }
+    }
+    return sortDominoes;
+  }
   static List<Domino> initializeDominoes() {
     List<Domino> dominoes = new ArrayList<>();
     dominoes.add(new Domino(5, 2));
+    System.out.println(dominoes.get(0).getValues()[1]);
     dominoes.add(new Domino(4, 6));
     dominoes.add(new Domino(1, 5));
     dominoes.add(new Domino(6, 7));
