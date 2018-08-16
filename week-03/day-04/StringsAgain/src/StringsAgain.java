4671b063f990e652bc92a932d033847de64a8fb1
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-16 16:43:33 +0200

    StringsAgainAndAgain finished

--- week-03/day-04/StringsAgain/src/StringsAgain.java
+++ week-03/day-04/StringsAgain/src/StringsAgain.java
@@ -1,14 +1,15 @@
 public class StringsAgain {
   public static void main(String[] args) {
     // Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
     System.out.println(stringsNoX("All the x'es have been removed from this sentence."));
   }
   public static String stringsNoX (String input) {
     if (input.length() == 0) {
       return input;
     } else {
-      String prefix = input.startsWith("x") ? "" : input.substring(0, 1);
-      return prefix + stringsNoX(input.substring(1));
+      String first = input.startsWith("x") ? "" : input.substring(0, 1);
+      return first + stringsNoX(input.substring(1));
     }
   }
 }
---
+++ week-03/day-04/StringsAgainAndAgain/src/StringsAgainAndAgain.java
@@ -0,0 +1,16 @@
+public class StringsAgainAndAgain {
+  public static void main(String[] args) {
+    // Given a string, compute recursively a new string where all the
+    // adjacent chars are now separated by a "*".
+
+    System.out.println(stringsAndStars("This is one of those days."));
+  }
+  public static String stringsAndStars (String input) {
+    if (input.isEmpty()) {
+      return input;
+    } else {
+      String fromSecond = input.substring(1);
+      return input.substring(0,1) + "*" + stringsAndStars(fromSecond);
+    }
+  }
+}
