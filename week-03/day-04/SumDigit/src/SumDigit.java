6e1115185abf078932fa36afe7c745ffe488a564
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-17 10:16:37 +0200

    SumDigit modified with extra method to sum digits of result as well

--- week-03/day-04/SumDigit/src/SumDigit.java
+++ week-03/day-04/SumDigit/src/SumDigit.java
@@ -5,12 +5,21 @@ public class SumDigit {
     // divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
 
     System.out.println(sum(1549));
+    System.out.println(sumToOneDigit(1549));
   }
   public static int sum (int a) {
     if (a < 10) {
       return a;
     } else {
       return sum(a / 10) + a % 10;
+    }
+  }
+  //Sums even the digits of the result until it is only one digit
+  public static int sumToOneDigit (int a) {
+    if (a < 10) {
+      return a;
+    } else {
+      return sumToOneDigit(sumToOneDigit(a / 10) + a % 10);
     }
   }
 }
