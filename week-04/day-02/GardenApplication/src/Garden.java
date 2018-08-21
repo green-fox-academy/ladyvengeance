1e23e08f82943c55c7b5880784015406035e2a26
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-21 17:10:49 +0200

    Garden application finally finished

--- week-04/day-02/GardenApplication/src/Garden.java
+++ week-04/day-02/GardenApplication/src/Garden.java
@@ -3,18 +3,31 @@ import java.util.ArrayList;
 public class Garden {
   ArrayList<Plant> plants = new ArrayList<>();
 
-  plants.add(flower01);
-    plants.add(flower02);
-    plants.add(tree01);
-    plants.add(tree02);
+  public void addPlant(Plant newPlant){
+    plants.add(newPlant);
+  }
 
   public void checkStatus() {
     for (int i = 0; i < plants.size(); i++) {
       if (plants.get(i) instanceof Flower && plants.get(i).needsWater() == true) {
         System.out.println("The " + plants.get(i).toString() + " Flower needs water");
+      } else if (plants.get(i) instanceof Flower && plants.get(i).needsWater() == false) {
+        System.out.println("The " + plants.get(i).toString() + " Flower doesn't need water");
       } else if (plants.get(i) instanceof Tree && plants.get(i).needsWater() == true) {
         System.out.println("The " + plants.get(i).toString() + " Tree needs water");
+      } else if (plants.get(i) instanceof Tree && plants.get(i).needsWater() == false) {
+        System.out.println("The " + plants.get(i).toString() + " Tree doesn't need water");
       }
+    }
+  }
+
+  public void waterGarden(int amountOfWater) {
+    int watered = 0;
+    for (int i = 0; i < plants.size(); i++) {
+      if (plants.get(i).needsWater() == false) {
+        watered ++;
+      }
+      plants.get(i).waterPlant(amountOfWater / (plants.size() - watered));
     }
   }
 }
--- week-04/day-02/GardenApplication/src/Main.java
+++ week-04/day-02/GardenApplication/src/Main.java
@@ -1,68 +1,29 @@
-import java.util.ArrayList;
-
 public class Main {
   public static void main(String[] args) {
     Garden garden = new Garden();
-
-    ArrayList<Plant> plants = new ArrayList<>();
 
     Flower flower01 = new Flower("yellow");
     Flower flower02 = new Flower("blue");
     Tree tree01 = new Tree("purple");
     Tree tree02 = new Tree("orange");
 
-    garden.add(flower01);
-    plants.add(flower02);
-    plants.add(tree01);
-    plants.add(tree02);
+    garden.addPlant(flower01);
+    garden.addPlant(flower02);
+    garden.addPlant(tree01);
+    garden.addPlant(tree02);
 
     garden.checkStatus();
+    System.out.println();
 
     System.out.println("Watering with 40");
-
-    for (int i = 0; i < plants.size(); i++) {
-      plants.get(i).waterPlant(40);
-    }
-
-    /*for (int i = 0; i < plants.size(); i++) {
-      if (plants.get(i) instanceof Flower && plants.get(i).needsWater() == true) {
-        System.out.println("The " + plants.get(i).toString() + " Flower needs water");
-      } else if (plants.get(i) instanceof Tree && plants.get(i).needsWater() == true) {
-        System.out.println("The " + plants.get(i).toString() + " Tree needs water");
-      }
-    }*/
-
-    System.out.println("Watering with 70");
-
-    for (int i = 0; i < plants.size(); i++) {
-      plants.get(i).waterPlant(70 / plants.size()) ;
-    }
+    garden.waterGarden(40);
 
     garden.checkStatus();
-/*
-    for (int i = 0; i < plants.size(); i++) {
-      if (plants.get(i) instanceof Flower && plants.get(i).needsWater() == true) {
-        System.out.println("The " + plants.get(i).toString() + " Flower needs water");
-      } else if (plants.get(i) instanceof Tree && plants.get(i).needsWater() == true) {
-        System.out.println("The " + plants.get(i).toString() + " Tree needs water");
-      }
-    }*/
+    System.out.println();
 
-/*    Watering with 40
-    The yellow Flower doesnt need water
-    The blue Flower doesnt need water
-    The purple Tree needs water
-    The orange Tree needs water
+    System.out.println("Watering with 70");
+    garden.waterGarden(70);
 
-    Watering with 70
-    The yellow Flower doesnt need water
-    The blue Flower doesnt need water
-    The purple Tree doesnt need water
-    The orange Tree doesnt need water*/
-
-    System.out.println(flower01.needsWater());
-    System.out.println(tree01.needsWater());
-    System.out.println(flower01.getWaterLevel());
-    System.out.println(tree01.getWaterLevel());
+    garden.checkStatus();
   }
 }
