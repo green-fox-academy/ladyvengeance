5c815b062034ef3cfd851a139f0b03cbd0a80bb4
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-21 15:53:22 +0200

    Garden application continued

--- week-04/day-02/GardenApplication/src/Main.java
+++ week-04/day-02/GardenApplication/src/Main.java
@@ -5,31 +5,45 @@ public class Main {
 
     Flower flower01 = new Flower("yellow");
     Flower flower02 = new Flower("blue");
-
-    ArrayList<Plant> flowers = new ArrayList<>();
-    flowers.add(flower01);
-    flowers.add(flower02);
-
     Tree tree01 = new Tree("purple");
     Tree tree02 = new Tree("orange");
 
-    ArrayList<Plant> trees = new ArrayList<>();
-    trees.add(tree01);
-    trees.add(tree02);
+    ArrayList<Plant> plants = new ArrayList<>();
+    plants.add(flower01);
+    plants.add(flower02);
+    plants.add(tree01);
+    plants.add(tree02);
 
-    for (int i = 0; i < flowers.size(); i++) {
-      if (flowers.get(i).needsWater() == true) {
-        System.out.println("The " + flowers.get(i).toString() + " Flower needs water");
+    for (int i = 0; i < plants.size(); i++) {
+      if (plants.get(i) instanceof Flower && plants.get(i).needsWater() == true) {
+        System.out.println("The " + plants.get(i).toString() + " Flower needs water");
+      } else if (plants.get(i) instanceof Tree && plants.get(i).needsWater() == true) {
+        System.out.println("The " + plants.get(i).toString() + " Tree needs water");
       }
     }
-
-    for (int i = 0; i < trees.size(); i++) {
-      if (trees.get(i).needsWater() == true) {
-        System.out.println("The " + trees.get(i).toString() + " Tree needs water");
-      }
+    
+    for (int i = 0; i < plants.size(); i++) {
+      plants.get(i).waterPlant(40);
     }
-//    System.out.println(flower01.needsWater());
+
+
+
+
+/*    Watering with 40
+    The yellow Flower doesnt need water
+    The blue Flower doesnt need water
+    The purple Tree needs water
+    The orange Tree needs water
+
+    Watering with 70
+    The yellow Flower doesnt need water
+    The blue Flower doesnt need water
+    The purple Tree doesnt need water
+    The orange Tree doesnt need water*/
+
+    System.out.println(flower01.needsWater());
     System.out.println(tree01.needsWater());
     System.out.println(flower01.getWaterLevel());
+    System.out.println(tree01.getWaterLevel());
   }
 }
