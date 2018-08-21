10cd79ad9015591f10f7954c786102e36d2d9de7
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-21 14:36:25 +0200

    Garden application continued

--- week-04/day-02/GardenApplication/src/Main.java
+++ week-04/day-02/GardenApplication/src/Main.java
@@ -3,24 +3,29 @@ import java.util.ArrayList;
 public class Main {
   public static void main(String[] args) {
 
-    Plant flower01 = new Flower("yellow");
-    Plant flower02 = new Flower("blue");
+    Flower flower01 = new Flower("yellow");
+    Flower flower02 = new Flower("blue");
 
     ArrayList<Plant> flowers = new ArrayList<>();
     flowers.add(flower01);
     flowers.add(flower02);
 
-    Plant tree01 = new Tree("purple");
-    Plant tree02 = new Tree("orange");
+    Tree tree01 = new Tree("purple");
+    Tree tree02 = new Tree("orange");
 
     ArrayList<Plant> trees = new ArrayList<>();
     trees.add(tree01);
     trees.add(tree02);
 
     for (int i = 0; i < flowers.size(); i++) {
-
-      if (flower01.needsWater() == true) {
+      if (flowers.get(i).needsWater() == true) {
         System.out.println("The " + flowers.get(i).toString() + " Flower needs water");
+      }
+    }
+
+    for (int i = 0; i < trees.size(); i++) {
+      if (trees.get(i).needsWater() == true) {
+        System.out.println("The " + trees.get(i).toString() + " Tree needs water");
       }
     }
 //    System.out.println(flower01.needsWater());
