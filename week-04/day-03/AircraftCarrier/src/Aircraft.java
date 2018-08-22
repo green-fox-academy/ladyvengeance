fb6b268f8999f90e502f379ebfc6c01054dfe563
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-22 21:54:17 +0200

    AircraftCarrier continued

--- week-04/day-03/AircraftCarrier/src/Aircraft.java
+++ week-04/day-03/AircraftCarrier/src/Aircraft.java
@@ -1,6 +1,22 @@
 public class Aircraft {
   private String type;
-  private int ammo;
+  private int ammo = 0;
+  private int maxAmmo;
+  private int baseDamage;
 
+  public int fight() {
+    int damage = baseDamage * ammo;
+    ammo = 0;
+    return damage;
+  }
 
+  public int refill(int ammoToFill) {
+    int extraAmmo = ammoToFill - maxAmmo;
+    return extraAmmo;
+  }
+
+  public String getType (String type) {
+    this.type = type;
+    return type.toString();
+  }
 }
