db73d02e76db66233ba6ae0ef378c784571af87c
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-22 13:59:28 +0200

    Cloneable corrected

--- week-04/day-03/Cloneable/src/Main.java
+++ week-04/day-03/Cloneable/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
   public static void main(String[] args) {
     Student student = new Student("John", 20, "male", "BME");
-    
+
     try {
-      student.clone();
+      Student student2 = (Student)student.clone();
     } catch (CloneNotSupportedException e) {
       e.printStackTrace();
     }
