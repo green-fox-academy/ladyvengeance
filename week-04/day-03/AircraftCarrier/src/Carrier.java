bd267679caa061ca0ee84d5cd731ca036bbe70f0
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-22 22:09:03 +0200

    AircraftCarrier Carrier class constructor finished

--- week-04/day-03/AircraftCarrier/src/Carrier.java
+++ week-04/day-03/AircraftCarrier/src/Carrier.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class Carrier {
+  ArrayList<Aircraft> mother = new ArrayList<>();
+  private int storeOfAmmo;
+  private int healthPoint;
+
+  public Carrier(int storeOfAmmo) {
+    this.storeOfAmmo = storeOfAmmo;
+    this.healthPoint = healthPoint;
+  }
 }
