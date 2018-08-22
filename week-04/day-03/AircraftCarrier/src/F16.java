54bda22295a2a94f640492391ebefc2043c40617
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-23 00:14:40 +0200

    AircraftCarrier project moved to day-02

--- week-04/day-03/AircraftCarrier/src/F16.java
+++
@@ -1,4 +0,0 @@
-public class F16 extends Aircraft {
-  private int maxAmmo = 8;
-  private int baseDamage = 30;
-}
--- week-04/day-03/AircraftCarrier/src/F35.java
+++
@@ -1,4 +0,0 @@
-public class F35 extends Aircraft {
-  private int maxAmmo = 12;
-  private int baseDamage = 50;
-}
