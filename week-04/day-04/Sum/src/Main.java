88ff5024ff0f231852e49d1c1d29091a977f59da
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-23 15:33:55 +0200

    Sum Main class finished

--- week-04/day-04/Sum/src/Main.java
+++ week-04/day-04/Sum/src/Main.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class Main {
+  public static void main(String[] args) {
+
+    ArrayList<Integer> al = new ArrayList<>();
+    al.add(1);
+    al.add(15);
+    al.add(25);
+    al.add(42);
+    al.add(3);
+    al.add(8);
+
+    System.out.println(sum(al));
+  }
+
+  public static int sum(ArrayList<Integer> input) {
+    int sum = 0;
+    for (Integer integer : input) {
+      sum += integer;
+    }
+    return sum;
+  }
 }
