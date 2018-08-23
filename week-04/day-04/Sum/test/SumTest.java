189be88e6d88229d5055ca506691d38ee0eaca96
ladyvengeance <csilla.deszpoth@gmail.com>
2018-08-23 17:06:43 +0200

    Sum finished

--- week-04/day-04/Sum/test/SumTest.java
+++ week-04/day-04/Sum/test/SumTest.java
@@ -1,4 +1,3 @@
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,19 +8,49 @@ public class SumTest {
   Sum s = new Sum();
   ArrayList<Integer> al = new ArrayList<>();
 
-  @Before
-  public void setUp() {
+  @Test
+  public void testSumAL() {
     al.add(1);
     al.add(2);
     al.add(3);
-  }
-
-  @Test
-  public void testSumAL() {
     int sum = 0;
+
     for (Integer integer : al) {
       sum += integer;
     }
     assertEquals(6, sum);
+  }
+
+  @Test
+  public void testSumALWithEmptyList() {
+    int sum = 0;
+
+    for (Integer integer : al) {
+      sum += integer;
+    }
+    assertEquals(0, sum);
+  }
+
+  @Test
+  public void testSumALWithOneElement() {
+    al.add(1);
+    int sum = 0;
+
+    for (Integer integer : al) {
+      sum += integer;
+    }
+    assertEquals(1, sum);
+  }
+
+  @Test(expected=NullPointerException.class)
+  public void testSumALWithNull() {
+    al = null;
+
+    int sum = 0;
+
+    for (Integer integer : al) {
+      sum += integer;
+    }
+    assertEquals(0, sum);
   }
 }
