23fc854ff49b9a68cdd9f7eb181f7c8380ab8b12
ladyvengeance <csilla.deszpoth@gmail.com>
2018-09-05 00:58:55 +0200

    Still working on Unique with Array

--- week-02/day-01/src/Unique.java
+++ week-02/day-01/src/Unique.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Unique {
   public static void main(String[] args) {
@@ -6,18 +7,31 @@ public class Unique {
     //  Returns a list of numbers where every number in the list occurs only once
 
     //  Example
-    System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
+    System.out.println(Arrays.toString(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34})));
     //  should print: `[1, 11, 34, 52, 61]`
 
   }
-  public static ArrayList<Integer> unique(int[] input) {
-    ArrayList<Integer> uniqueList = new ArrayList<>();
+
+//  public static ArrayList<Integer> unique(int[] input) {
+//    ArrayList<Integer> uniqueList = new ArrayList<>();
+//
+//    for (int i = 0; i < input.length; i++) {
+//        if (!uniqueList.contains(input[i])) {
+//          uniqueList.add(input[i]);
+//        }
+//    }
+//    return uniqueList;
+//  }
+  public static int[] unique(int[] input) {
+    int[] uniqueArray = new int[input.length];
 
     for (int i = 0; i < input.length; i++) {
-        if (!uniqueList.contains(input[i])) {
-          uniqueList.add(input[i]);
+      for (int j = 0; j <= i; j++) {
+        if (uniqueArray[j] != input[i]) {
+          uniqueArray[i] = input[i];
         }
+      }
     }
-    return uniqueList;
+    return uniqueArray;
   }
 }
