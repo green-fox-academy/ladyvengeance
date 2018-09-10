95b7ce41d13585b17ccffb774bb16fdb31e23304
ladyvengeance <csilla.deszpoth@gmail.com>
2018-09-10 17:19:31 +0200

    Ex05 modified

--- week-06/day-01/Exercise05/src/Exercise05.java
+++ week-06/day-01/Exercise05/src/Exercise05.java
@@ -1,3 +1,5 @@
+import java.util.stream.Collectors;
+
 public class Exercise05 {
   public static void main(String[] args) {
 //    Write a Stream Expression to find the uppercase characters in a string!
@@ -14,10 +16,16 @@ public class Exercise05 {
 
     String onlyUps = whatever.chars()
         .filter(s -> Character.isUpperCase(s))
-        .collect(StringBuilder::new,
-            StringBuilder::appendCodePoint,
-            StringBuilder::append)
-        .toString();
+        .mapToObj(s -> Character.toString((char) s))
+        .collect(Collectors.joining(" "));
+
+//    Alternate version, was unable to add spaces after each character with the below
+//    String onlyUps = whatever.chars()
+//        .filter(s -> Character.isUpperCase(s))
+//        .collect(StringBuilder::new,
+//            StringBuilder::appendCodePoint,
+//            StringBuilder::append)
+//        .toString();
     System.out.println(onlyUps);
   }
 }
