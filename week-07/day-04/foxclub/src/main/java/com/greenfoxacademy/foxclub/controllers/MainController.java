4c758f692c58121f90b84a69912a7bb0a3c77bfd
ladyvengeance <csilla.deszpoth@gmail.com>
2018-09-20 10:29:56 +0200

    fixed index.html, greenfox.png now displayed

--- week-07/day-04/foxclub/src/main/java/com/greenfoxacademy/foxclub/controllers/MainController.java
+++ week-07/day-04/foxclub/src/main/java/com/greenfoxacademy/foxclub/controllers/MainController.java
@@ -1,7 +1,13 @@
 package com.greenfoxacademy.foxclub.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+  @GetMapping("/")
+  public String getMainPage() {
+    return "index";
+  }
 }
