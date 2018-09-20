af34d9d0a2a90bc79b5ab588bb0f0b05be016140
ladyvengeance <csilla.deszpoth@gmail.com>
2018-09-20 14:05:20 +0200

    added Fox class with name attribute and corresponding getters and setters

---
+++ week-07/day-04/foxclub/src/main/java/com/greenfoxacademy/foxclub/services/Fox.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.foxclub.services;
+
+public class Fox {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
