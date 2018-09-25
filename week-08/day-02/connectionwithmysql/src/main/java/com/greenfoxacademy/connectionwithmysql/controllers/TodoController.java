299b39499bb80b395eafce62ee078197af477dc4
ladyvengeance <csilla.deszpoth@gmail.com>
2018-09-25 14:35:28 +0200

    recreated previous exercise

---
+++ week-08/day-02/connectionwithmysql/src/main/java/com/greenfoxacademy/connectionwithmysql/controllers/TodoController.java
@@ -0,0 +1,29 @@
+package com.greenfoxacademy.connectionwithmysql.controllers;
+
+import com.greenfoxacademy.connectionwithmysql.repositories.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TodoController {
+
+  private TodoRepository todoRepository;
+
+  @Autowired
+  public TodoController(TodoRepository todoRepository) {
+    this.todoRepository = todoRepository;
+  }
+
+  @GetMapping("/todo")
+  public String showTodo() {
+    return "todo";
+  }
+
+  @GetMapping(value = {"/", "/list"})
+  public String list(Model model) {
+    model.addAttribute("todos", todoRepository.findAll());
+    return "todolist";
+  }
+}
