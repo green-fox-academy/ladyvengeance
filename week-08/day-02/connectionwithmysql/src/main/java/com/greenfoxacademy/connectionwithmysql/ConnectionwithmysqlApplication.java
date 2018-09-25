b29d31f4f7d485fe7c873bcf85bf02a7fe65bc9f
ladyvengeance <csilla.deszpoth@gmail.com>
2018-09-25 15:41:39 +0200

    fixed application class

--- week-08/day-02/connectionwithmysql/src/main/java/com/greenfoxacademy/connectionwithmysql/ConnectionwithmysqlApplication.java
+++ week-08/day-02/connectionwithmysql/src/main/java/com/greenfoxacademy/connectionwithmysql/ConnectionwithmysqlApplication.java
@@ -1,12 +1,24 @@
 package com.greenfoxacademy.connectionwithmysql;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+    import com.greenfoxacademy.connectionwithmysql.models.Todo;
+    import com.greenfoxacademy.connectionwithmysql.repositories.TodoRepository;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.boot.CommandLineRunner;
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConnectionwithmysqlApplication {
+public class ConnectionwithmysqlApplication implements CommandLineRunner {
+
+  @Autowired
+  private TodoRepository todoRepository;
 
   public static void main(String[] args) {
     SpringApplication.run(ConnectionwithmysqlApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    todoRepository.save(new Todo("I have to learn Object Relational Mapping"));
   }
 }
