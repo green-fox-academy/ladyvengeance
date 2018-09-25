ea90b9835bddd0b2ee966f1542813fcce5ade965
ladyvengeance <csilla.deszpoth@gmail.com>
2018-09-25 14:11:40 +0200

    todolist.html added

--- week-08/day-02/springwithsql/src/main/java/com/greenfoxacademy/springwithsql/controllers/TodoController.java
+++ week-08/day-02/springwithsql/src/main/java/com/greenfoxacademy/springwithsql/controllers/TodoController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TodoController {
@@ -23,8 +22,8 @@ public class TodoController {
   }
 
   @GetMapping(value = {"/", "/list"})
-  @ResponseBody
   public String list(Model model) {
-    return "This is my first todo";
+    model.addAttribute("todos", todoRepository.findAll());
+    return "todolist";
   }
 }
