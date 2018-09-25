25ee5118bd3b321740746fe3670ae29091186b60
ladyvengeance <csilla.deszpoth@gmail.com>
2018-09-25 13:43:44 +0200

    added tricks inside Fox constructor

--- week-07/day-04/foxclub/src/main/java/com/greenfoxacademy/foxclub/controllers/MainController.java
+++ week-07/day-04/foxclub/src/main/java/com/greenfoxacademy/foxclub/controllers/MainController.java
@@ -1,6 +1,5 @@
 package com.greenfoxacademy.foxclub.controllers;
 
-import com.greenfoxacademy.foxclub.models.Fox;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
