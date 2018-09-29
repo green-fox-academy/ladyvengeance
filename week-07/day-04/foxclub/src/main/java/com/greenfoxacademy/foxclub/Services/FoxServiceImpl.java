98de9074076d82bd70c66355f62d08ac6651b522
ladyvengeance <csilla.deszpoth@gmail.com>
2018-09-29 16:49:52 +0200

    added autowiring in FoxServiceImpl

--- week-07/day-04/foxclub/src/main/java/com/greenfoxacademy/foxclub/Services/FoxServiceImpl.java
+++ week-07/day-04/foxclub/src/main/java/com/greenfoxacademy/foxclub/Services/FoxServiceImpl.java
@@ -2,6 +2,7 @@ package com.greenfoxacademy.foxclub.Services;
 
 import com.greenfoxacademy.foxclub.models.Fox;
 import com.greenfoxacademy.foxclub.repositories.FoxRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,6 +10,7 @@ public class FoxServiceImpl implements FoxService {
 
   private FoxRepository foxRepository;
 
+  @Autowired
   public FoxServiceImpl(FoxRepository foxRepository) {
     this.foxRepository = foxRepository;
   }
