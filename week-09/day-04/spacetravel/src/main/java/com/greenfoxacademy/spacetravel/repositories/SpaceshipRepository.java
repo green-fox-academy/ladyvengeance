b4db2ea86ef3a31adada0f8289a349d3d6089bb8
ladyvengeance <csilla.deszpoth@gmail.com>
2018-10-04 16:31:28 +0200

    deleted day-04

--- week-09/day-04/spacetravel/src/main/java/com/greenfoxacademy/spacetravel/repositories/SpaceshipRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greenfoxacademy.spacetravel.repositories;
-
-import com.greenfoxacademy.spacetravel.models.Spaceship;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SpaceshipRepository extends CrudRepository<Spaceship, Long> {
-}
