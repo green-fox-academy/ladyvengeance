2f767ec4aa6430a9d4d657b6fc1071918d78475a
ladyvengeance <csilla.deszpoth@gmail.com>
2018-10-04 15:30:32 +0200

    added index.html

--- week-09/day-04/spacetravel/src/main/java/com/greenfoxacademy/spacetravel/controllers/SpaceController.java
+++ week-09/day-04/spacetravel/src/main/java/com/greenfoxacademy/spacetravel/controllers/SpaceController.java
@@ -3,6 +3,7 @@ package com.greenfoxacademy.spacetravel.controllers;
 import com.greenfoxacademy.spacetravel.services.SpaceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SpaceController {
@@ -12,5 +13,10 @@ public class SpaceController {
   @Autowired
   public SpaceController(SpaceService spaceService) {
     this.spaceService = spaceService;
+  }
+
+  @GetMapping("")
+  public String showIndex() {
+    return "index";
   }
 }
